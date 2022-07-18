57d5af31130f0e781cd3955b2b5601f51e27ffc9
MaiEmadd <maiemad666@gmail.com>
2022-07-18 10:40:47 +0200

    structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatTableModule} from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './contracts/index/index.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {MatInputModule } from '@angular/material/input';
 import {MatSelectModule } from '@angular/material/select';
