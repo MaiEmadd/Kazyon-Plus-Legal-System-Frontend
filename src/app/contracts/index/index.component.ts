6bf66307a156dad92d11750fffae2b392bc919f9
Mahmoud Amr <engineer.mahmoudamr@gmail.com>
2022-07-31 10:19:46 +0200

    Testing Issues Fix

--- src/app/contracts/index/index.component.ts
+++ src/app/contracts/index/index.component.ts
@@ -8,6 +8,7 @@ import { MatSelect
 import { Router } from '@angular/router';
 import { Contract } from '../contract';
 import { MatIcon } from '@angular/material/icon';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-index',
@@ -15,7 +16,7 @@ import { MatIcon } from '@angular/material/icon';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-
+  // Class variables
   statusSummary!:ContractSummary;
   apidata:any[] =[];
   dataSource:any[] =[];
@@ -23,6 +24,7 @@ export class IndexComponent implements OnInit {
   unique_branch_name :any[] =[]
   unique_province: any[] = []
   unique_status:any[]=[];
+  ascending_sort : Boolean | null = null;
 
   filter_object  = {
     branch!: "",
@@ -30,8 +32,9 @@ export class IndexComponent implements OnInit {
     province!: "",
     status!: ""
   }
+  // End of class vairables
 
-
+  // Column display
   displayedColumns: string[] = ['كود التشغيل', 'الفرع', 'المحافظة', 'الاقليم','الحالة'];
 
   getAllContracts(filter_string:String) {
@@ -47,6 +50,25 @@ export class IndexComponent implements OnInit {
 
 
   }
+  // End of column display
+
+  // Ordering of rows
+  ascending(a :any,b :any) {
+    if (a.store_code < b.store_code)
+       return -1;
+    if (a.store_code > b.store_code)
+      return 1;
+    return 0;
+  }
+
+  descending(a :any,b :any) {
+    if (a.store_code > b.store_code)
+       return -1;
+    if (a.store_code < b.store_code)
+      return 1;
+    return 0;
+  }
+  // End ordering
 
   @ViewChild(MatSort) sort =new MatSort();
   constructor(private _ContractsApiService:ContractsApiService, private _router:Router) {
@@ -58,16 +80,9 @@ export class IndexComponent implements OnInit {
 
 
 
-   statusFilter($event:any,apidata:any){
-    let filterDate = _.filter(apidata,(item)=>{
-      return item.status.toLowerCase() == $event.value.toLowerCase();
-    })
-    this.dataSource = filterDate;
-   }
 
-
-
-   changeFilter($event:any, type:String) {
+  //  Filter and Sort
+  changeFilter($event:any, type:String) {
     let value = $event.value;
 
     if (this.filter_object[type as keyof typeof this.filter_object] == value) {
@@ -91,11 +106,23 @@ export class IndexComponent implements OnInit {
       this.sendApiRequest();
    }
 
-   sendApiRequest() {
+  sortEvent(table?: MatTable<any>) {
+    if (!this.ascending_sort)
+      this.dataSource = this.dataSource.sort(this.ascending);
+    else
+      this.dataSource = this.dataSource.sort(this.descending)
+
+    this.ascending_sort = !this.ascending_sort;
+    table?.renderRows()
+  }
+  //  End of filter and sort
+
+  sendApiRequest() {
     this.getAllContracts(`governorate=${this.filter_object.governorate}&status=${this.filter_object.status}&province=${this.filter_object.province}&branch_name=${this.filter_object.branch}`);
    }
 
    display(id ?: number) {
+
     this._router.navigate(["contracts",id])
    }
 
